ac5b3a080de1eecacfe954b9a2b4262ea8a7bc00
Vishal Tomar <vishal@auquan.com>
2020-06-07 19:31:46 +0530

    slight changes in test

--- test/disease_model/ensemble_model/test_ensemble_model.py
+++ test/disease_model/ensemble_model/test_ensemble_model.py
@@ -6,15 +6,15 @@ from src.disease_model.ensemble_model.ensemble_model import EnsembleModel
 
 
 def test_ensemble_model():
-    vec_created = False
-
+    """
+    end to end test for disease model
+    """
     try:
         model = EnsembleModel(country="India")
         health_vector = model.get_health_status()
-        if len(health_vector)>0:
+        if len(health_vector) > 0:
             test = True
-    except Exception as inst:
-        # TODO: change to logging
-        print("exitstrategies test failed with exception:\n", inst)
-
+    except Exception as excep:
+        print("disease model not working:\n", excep)
+        test = False
     assert test
